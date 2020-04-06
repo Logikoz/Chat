358e7419783a5db2313df822ae0e1e8a314c29d7
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-04-06 17:48:20 -0500

    ✨ Enviando mensagem no chat [Web]

---
+++ Chat.Client.Web/Utils/MessageUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Chat.Client.Web.Utils
+{
+    public class MessageUtil<TAuthor, TMessage>
+    {
+        private TAuthor[] authors = new TAuthor[10000];
+        private TMessage[] messages = new TMessage[10000];
+        private uint currentIndex;
+
+        public TAuthor Author { get; set; }
+        public TMessage Message { get; set; }
+
+        public void Add(TAuthor author, TMessage message)
+        {
+            if (currentIndex >= authors.Length)
+                throw new IndexOutOfRangeException();
+
+            authors[currentIndex] = author;
+            messages[currentIndex] = message;
+            currentIndex++;
+        }
+    }
+}
