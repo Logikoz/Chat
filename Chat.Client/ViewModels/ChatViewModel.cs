63f9a4695fca9aee2b89a253d2b007e64bb5a23f
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-21 17:44:47 -0500

    Disparando command ao fechar a janela [Desktop]

--- Chat.Client/ViewModels/ChatViewModel.cs
+++ Chat.Client/ViewModels/ChatViewModel.cs
@@ -8,74 +8,77 @@ using System.Windows.Controls;
 
 namespace Chat.Client.ViewModels
 {
-    internal class ChatViewModel : ViewModelBase
-    {
-        private HubConnection hubConnection;
-        private string _message;
-        private string _userName;
-        private readonly RichTextBox _chat;
+	internal class ChatViewModel : ViewModelBase
+	{
+		private HubConnection hubConnection;
+		private string _message;
+		private string _userName;
+		private readonly RichTextBox _chat;
 
-        public string Message
-        {
-            get => _message;
-            set => Set(ref _message, value);
-        }
+		public string Message
+		{
+			get => _message;
+			set => Set(ref _message, value);
+		}
 
-        public string UserName
-        {
-            get => _userName;
-            set => Set(ref _userName, value);
-        }
+		public string UserName
+		{
+			get => _userName;
+			set => Set(ref _userName, value);
+		}
 
-        public RelayCommand ConnectCommand { get; set; }
-        public RelayCommand DesconnectCommand { get; set; }
-        public RelayCommand SendMessageCommand { get; set; }
+		public RelayCommand ConnectCommand { get; set; }
+		public RelayCommand DesconnectCommand { get; set; }
+		public RelayCommand SendMessageCommand { get; set; }
 
-        public ChatViewModel(RichTextBox chat)
-        {
-            Init();
+		public ChatViewModel(RichTextBox chat)
+		{
+			Init();
 
-            _chat = chat;
-        }
+			_chat = chat;
+		}
 
-        private void Init()
-        {
-            hubConnection = new HubConnectionBuilder().WithUrl("http://logikoz.ddns.net:5000/ChatHub").Build();
+		private void Init()
+		{
+			hubConnection = new HubConnectionBuilder().WithUrl("http://192.168.1.6:5000/ChatHub").Build();
 
-            hubConnection.On<string, string>("ReceiveMessage", (user, message) => SendMessageChat($"{user}: {message}"));
+			hubConnection.On<string, string>("ReceiveMessage", (user, message) => SendMessageChat($"{user}: {message}"));
 
-            hubConnection.On<string>("JoinChat", (user) => SendMessageChat($"{user} entrou no chat!"));
+			hubConnection.On<string>("JoinChat", (user) => SendMessageChat($"{user} entrou no chat!"));
 
-            hubConnection.On<string>("LeaveChat", (user) => SendMessageChat($"{user} saiu do chat!"));
+			hubConnection.On<string>("LeaveChat", (user) => SendMessageChat($"{user} saiu do chat!"));
 
-            void SendMessageChat(string msg)
-            {
-                _chat.AppendText(msg + "\n");
-                _chat.ScrollToEnd();
-            }
+			void SendMessageChat(string msg)
+			{
+				_chat.AppendText(msg + "\n");
+				_chat.ScrollToEnd();
+			}
 
-            //commands
-            ConnectCommand = new RelayCommand(async () => await ConnectAsync());
-            DesconnectCommand = new RelayCommand(async () => await DesconnectAsync());
-            SendMessageCommand = new RelayCommand(async () => await SendMessageAsync());
-        }
+			//commands
+			ConnectCommand = new RelayCommand(async () => await ConnectAsync());
+			DesconnectCommand = new RelayCommand(async () => await DesconnectAsync());
+			SendMessageCommand = new RelayCommand(async () => await SendMessageAsync());
+		}
 
-        private async Task SendMessageAsync()
-        {
-            await hubConnection.InvokeAsync("SendMessage", UserName, Message);
-            Message = null;
-        }
+		private async Task SendMessageAsync()
+		{
+			await hubConnection.InvokeAsync("SendMessage", UserName, Message);
+			Message = null;
+		}
 
-        private async Task DesconnectAsync()
-        {
-            await hubConnection.InvokeAsync("LeaveChat", UserName);
-            await hubConnection.StopAsync();
-        }
+		private async Task DesconnectAsync()
+		{
+			if (hubConnection.State == HubConnectionState.Disconnected)
+			{
+				await hubConnection.InvokeAsync("LeaveChat", UserName);
+				await hubConnection.StopAsync();
+			}
+		}
 
-        private async Task ConnectAsync()
-        {
-            await hubConnection.StartAsync();
-            await hubConnection.InvokeAsync("JoinChat", UserName);
-        }
-    }
+		private async Task ConnectAsync()
+		{
+			await hubConnection.StartAsync();
+			await hubConnection.InvokeAsync("JoinChat", UserName);
+		}
+	}
 }
--- Chat.Client/Views/ChatView.xaml.cs
+++ Chat.Client/Views/ChatView.xaml.cs
@@ -4,12 +4,17 @@ using System.Windows;
 
 namespace Chat.Client.Views
 {
-    public partial class ChatView : Window
-    {
-        public ChatView()
-        {
-            InitializeComponent();
-            DataContext = new ChatViewModel(Chat);
-        }
-    }
+	public partial class ChatView : Window
+	{
+		public ChatView()
+		{
+			InitializeComponent();
+			DataContext = new ChatViewModel(Chat);
+			Closing += (sender, e) =>
+			{
+				var viewmodel = DataContext as ChatViewModel;
+				viewmodel.DesconnectCommand.Execute(null);
+			};
+		}
+	}
 }
