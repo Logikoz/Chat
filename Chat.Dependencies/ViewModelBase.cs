433d8d6cbc619e8a00cde23923eab7f619ae1357
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-21 19:54:35 -0500

    Criando ViewModelBase [Dependencies]

---
+++ Chat.Dependencies/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Chat.Dependencies
+{
+	public class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void Set<T>(ref T objectReferente, T newObjectValue, [CallerMemberName] string property = null)
+		{
+			objectReferente = newObjectValue;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+		}
+	}
+}
