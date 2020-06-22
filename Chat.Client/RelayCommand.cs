ef8eff4cc63b782d2e37577834ff6db48c5a1a6b
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-21 19:55:45 -0500

    Criando RelayCommand [Desktop]

---
+++ Chat.Client/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Chat.Client
+{
+	public class RelayCommand : ICommand
+	{
+		private readonly Action _execute;
+		private readonly Func<bool> _canExecute;
+
+		public RelayCommand(Action action) : this(action, null)
+		{
+		}
+
+		public RelayCommand(Action action, Func<bool> canExecute)
+		{
+			if (action is null)
+				throw new ArgumentNullException("action is null");
+
+			_execute = action;
+
+			if (canExecute != null)
+				_canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter) =>
+			_canExecute == null || _canExecute();
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public void Execute(object parameter) =>
+			_execute();
+	}
+}
---
+++ Chat.Client/RelayCommandGeneric.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Chat.Client
+{
+	public class RelayCommand<T> : ICommand
+	{
+		private readonly Action<T> _execute;
+		private readonly Func<T, bool> _canExecute;
+
+		public RelayCommand(Action<T> action) : this(action, null)
+		{
+		}
+
+		public RelayCommand(Action<T> action, Func<T, bool> canExecute)
+		{
+			if (action is null)
+				throw new NullReferenceException("action is null");
+
+			_execute = action;
+
+			if (canExecute != null)
+				_canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter) =>
+			_canExecute != null && _canExecute((T)parameter);
+
+		public event EventHandler CanExecuteChanged
+		{
+			add
+			{
+				CommandManager.RequerySuggested += value;
+			}
+			remove
+			{
+				CommandManager.RequerySuggested -= value;
+			}
+		}
+
+		public void Execute(object parameter) =>
+			_execute((T)parameter);
+	}
+}
